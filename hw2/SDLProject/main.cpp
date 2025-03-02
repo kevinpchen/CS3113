4fc075aca691affea49bb37ccdacf698632a9fa2
KC <150204442+kevinpchen@users.noreply.github.com>
2025-03-01 19:28:47 -0500

    Update main.cpp

--- hw2/SDLProject/main.cpp
+++ hw2/SDLProject/main.cpp
@@ -1,7 +1,7 @@
 /**
 * Author: Kevin Chen
-* Assignment: Simple 2D Scene
-* Date due: 2025-02-15, 11:59pm
+* Assignment: Pong Clone
+* Date due: 2025-3-01, 11:59pm
 * I pledge that I have completed this assignment without
 * collaborating with anyone else, in conformance with the
 * NYU School of Engineering Policies and Procedures on
